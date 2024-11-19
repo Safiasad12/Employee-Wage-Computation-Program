940ee0903a4618f5ba1dbdd83912faf1c4cce934
safi <safiasad12@gmail.com>
2024-11-20 00:53:54 +0530

    [safi] add: calculate daily wage

--- file.js
+++ file.js
@@ -1,9 +1,15 @@
 console.log("Welcom to Employee Wage Computation Program...");
 
+const wagePerHour = 20;  // Wage per hour in currency
+const fullDayHours = 8;  // Full day work hours
+
 let attendance = Math.floor(Math.random() * 2); // Generates 0 or 1
 
 if (attendance === 0) {
     console.log("Employee is Absent");
 } else {
     console.log("Employee is Present");
+    let dailyWage = wagePerHour * fullDayHours;
+    console.log("Employee's Daily Wage: " + dailyWage);
 }
+
