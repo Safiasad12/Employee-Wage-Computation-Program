3ed32887716bf5911da8cdfe008a929f5e5cc83e
safi <safiasad12@gmail.com>
2024-11-20 00:49:03 +0530

    [safi] add: check attendence

--- file.js
+++ file.js
@@ -1,1 +1,9 @@
 console.log("Welcom to Employee Wage Computation Program...");
+
+let attendance = Math.floor(Math.random() * 2); // Generates 0 or 1
+
+if (attendance === 0) {
+    console.log("Employee is Absent");
+} else {
+    console.log("Employee is Present");
+}
