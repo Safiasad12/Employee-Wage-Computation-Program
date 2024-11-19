67701119987035288382631fac4c86c4c1008d90
safi <safiasad12@gmail.com>
2024-11-20 01:05:36 +0530

    [safi] add: part time employee

--- file.js
+++ file.js
@@ -2,14 +2,41 @@ console.log("Welcom to Employee Wage Computation Program...");
 
 const wagePerHour = 20;  // Wage per hour in currency
 const fullDayHours = 8;  // Full day work hours
+const halfDayHours = 4;
 
 let attendance = Math.floor(Math.random() * 2); // Generates 0 or 1
 
 if (attendance === 0) {
     console.log("Employee is Absent");
-} else {
+}
+if (attendance === 1) {
     console.log("Employee is Present");
-    let dailyWage = wagePerHour * fullDayHours;
-    console.log("Employee's Daily Wage: " + dailyWage);
+    let employeeType = Math.floor(Math.random() * 2);
+    if (employeeType === 0) {
+        console.log("Part-Time Employee");
+        let dailyWage = wagePerHour * halfDayHours;
+        console.log("Employee's Daily Wage: " + dailyWage);
+    }
+    if (employeeType === 1) {
+        console.log("Full-Time Employee");
+        let dailyWage = wagePerHour * fullDayHours;
+        console.log("Employee's Daily Wage: " + dailyWage);
+    }
 }
 
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+    
