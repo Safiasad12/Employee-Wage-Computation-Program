87a84e4f105e8c25dc27e4b83396b80875502abb
safi <safiasad12@gmail.com>
2024-11-20 01:34:01 +0530

    [safi] add: wages for a month

--- file.js
+++ file.js
@@ -3,49 +3,41 @@ console.log("Welcom to Employee Wage Computation Program...");
 const wagePerHour = 20;  // Wage per hour in currency
 const fullDayHours = 8;  // Full day work hours
 const halfDayHours = 4;
+const totalWorkigDaysForAMonth = 20;
 
-let attendance = Math.floor(Math.random() * 2); // Generates 0 or 1
+for (let day = 1; day <= totalWorkigDaysForAMonth; day++) {
 
-// if (attendance === 0) {
-//     console.log("Employee is Absent");
-// }
-// if (attendance === 1) {
-//     console.log("Employee is Present");
-//     let employeeType = Math.floor(Math.random() * 2);
-//     if (employeeType === 0) {
-//         console.log("Part-Time Employee");
-//         let dailyWage = wagePerHour * halfDayHours;
-//         console.log("Employee's Daily Wage: " + dailyWage);
-//     }
-//     if (employeeType === 1) {
-//         console.log("Full-Time Employee");
-//         let dailyWage = wagePerHour * fullDayHours;
-//         console.log("Employee's Daily Wage: " + dailyWage);
-//     }
-// }
+    console.log(`Day ${day}`);
 
-let dailyWage;
+    let attendance = Math.floor(Math.random() * 2); // Generates 0 or 1
 
-switch (attendance) {
-    case 0:
-        console.log("Employee is Absent");
-        break;
-    case 1:
-        console.log("Employee is Present");
-        let employeeType = Math.floor(Math.random() * 2);
-        switch (employeeType) {
-            case 0:
-                console.log("Part-Time Employee");
-                dailyWage = wagePerHour * halfDayHours;
-                console.log("Employee's Daily Wage: " + dailyWage);
-                break;
-            case 1:
-                console.log("Full-Time Employee");
-                dailyWage = wagePerHour * fullDayHours;
-                console.log("Employee's Daily Wage: " + dailyWage);
-                break;
-        }
+    let dailyWage;
+
+    switch (attendance) {
+        case 0:
+            console.log("Employee is Absent");
+            break;
+        case 1:
+            console.log("Employee is Present");
+            let employeeType = Math.floor(Math.random() * 2);
+            switch (employeeType) {
+                case 0:
+                    console.log("Part-Time Employee");
+                    dailyWage = wagePerHour * halfDayHours;
+                    console.log("Employee's Daily Wage: " + dailyWage);
+                    break;
+                case 1:
+                    console.log("Full-Time Employee");
+                    dailyWage = wagePerHour * fullDayHours;
+                    console.log("Employee's Daily Wage: " + dailyWage);
+                    break;
+            }
+    }
+
+    console.log("-----------------------------------");
+
 }
+
 
 
 
